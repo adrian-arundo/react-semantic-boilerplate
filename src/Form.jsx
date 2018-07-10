4967851c16950c1fe05a50b6a3b1ecf5cb3e04cc
adrian-arundo <adrian.martinez@arundo.com>
2018-07-10 16:52:33 -0500

    First attempt

---
+++ src/Form.jsx
@@ -0,0 +1,67 @@
+import "styling/semantic.less";
+
+import React, { Component } from "react";
+import { Form, Container, Header } from "semantic-ui-react";
+
+const options = [
+  { key: "m", text: "Male", value: "male" },
+  { key: "f", text: "Female", value: "female" }
+];
+
+class FormExampleSubcomponentControl extends Component {
+  state = {};
+
+  handleChange = (e, { value }) => this.setState({ value });
+
+  render() {
+    const { value } = this.state;
+    return (
+      <Container style={{ marginTop: "3em" }}>
+        <Header as="h2" dividing>
+          Form
+        </Header>
+        <Form>
+          <Form.Group widths="equal">
+            <Form.Input fluid label="First name" placeholder="First name" />
+            <Form.Input fluid label="Last name" placeholder="Last name" />
+            <Form.Select
+              fluid
+              label="Gender"
+              options={options}
+              placeholder="Gender"
+            />
+          </Form.Group>
+          <Form.Group inline>
+            <label>Size</label>
+            <Form.Radio
+              label="Small"
+              value="sm"
+              checked={value === "sm"}
+              onChange={this.handleChange}
+            />
+            <Form.Radio
+              label="Medium"
+              value="md"
+              checked={value === "md"}
+              onChange={this.handleChange}
+            />
+            <Form.Radio
+              label="Large"
+              value="lg"
+              checked={value === "lg"}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.TextArea
+            label="About"
+            placeholder="Tell us more about you..."
+          />
+          <Form.Checkbox label="I agree to the Terms and Conditions" />
+          <Form.Button>Submit</Form.Button>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default FormExampleSubcomponentControl;
